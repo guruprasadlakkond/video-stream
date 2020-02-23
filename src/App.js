8cd7ef66a80dda09797254407474139bc583cb43
guruprasadlakkond <guruprasadlakkond@gmail.com>
2020-02-24 00:40:21 +0530

    oauth integration

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import StreamCreate from './components/streams/StreamCreate';
+import StreamEdit from './components/streams/StreamEdit';
+import StreamList from './components/streams/StreamList';
+import StreamDelete from './components/streams/StreamDelete';
+import StreamShow from './components/streams/StreamShow';
+import Header from './components/Header';
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Route path="/" exact component={StreamList} />
+        <Route path="/streams/new" exact component={StreamCreate} />
+        <Route path="/streams/edit" exact component={StreamEdit} />
+        <Route path="/streams/delete" exact component={StreamDelete} />
+        <Route path="/streams/show" exact component={StreamShow} />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
