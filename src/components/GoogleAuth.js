58ce2d8fc92a451ee5ce15994d6b42372b406b30
guruprasadlakkond <guruprasadlakkond@gmail.com>
2020-03-03 00:25:08 +0530

    Updated str to const globally

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -3,9 +3,6 @@ import { connect } from 'react-redux';
 import { signIn, signOut } from '../actions';
 
 class GoogleAuth extends React.Component {
-  state = {
-    isSignedIn: null
-  };
   componentDidMount() {
     window.gapi.load('client:auth2', () => {
       window.gapi.client
@@ -16,7 +13,7 @@ class GoogleAuth extends React.Component {
         })
         .then(() => {
           this.auth = window.gapi.auth2.getAuthInstance();
-          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+          this.onAuthChange(this.auth.isSignedIn.get());
           this.auth.isSignedIn.listen(this.onAuthChange);
         });
     });
@@ -24,7 +21,7 @@ class GoogleAuth extends React.Component {
 
   onAuthChange = isSignedIn => {
     if (isSignedIn) {
-      this.props.signIn();
+      this.props.signIn(this.auth.currentUser.get().getId());
     } else {
       this.props.signOut();
     }
@@ -39,9 +36,9 @@ class GoogleAuth extends React.Component {
   };
 
   renderAuthButton() {
-    if (this.state.isSignedIn === null) {
+    if (this.props.isSignedIn === null) {
       return <div>I am not sure </div>;
-    } else if (this.state.isSignedIn) {
+    } else if (this.props.isSignedIn) {
       return (
         <button className="ui red google button" onClick={this.signOut}>
           <i className="google icon" />
